9a3d19e80a42931b81ae39b9b440ed040f0f89b5
rizkynindra <rizkynindra@gmail.com>
2025-03-08 16:55:28 +0700

    malam jumat kliwon

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ torch.classes.__path__ = []
 
 
 inf = solutions.Inference(
-    model="model/best.pt",  # You can use any model that Ultralytics support, i.e. YOLO11, or custom trained model
+    model="model/best2.pt",  # You can use any model that Ultralytics support, i.e. YOLO11, or custom trained model
 )
 
 inf.inference()
