72687b10629aafac6e123827e627d0f897100285
rizkynindra <rizkynindra@gmail.com>
2025-03-08 15:01:14 +0700

    malam jumat kliwon

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from ultralytics import solutions
+import os
+import torch
+import streamlit
+
+torch.classes.__path__ = [os.path.join(torch.__path__[0], torch.classes.__file__)]
+
+# or simply:
+torch.classes.__path__ = []
+
 
 inf = solutions.Inference(
     model="model/best.pt",  # You can use any model that Ultralytics support, i.e. YOLO11, or custom trained model
